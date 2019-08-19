f5b3954c2d53b15994334b503aee73d98734acb0
nafetsrybak <nafetsrybak@gmail.com>
2019-08-19 20:31:13 +0300

    add function fileAction

--- module/Application/src/Controller/ImageController.php
+++ module/Application/src/Controller/ImageController.php
@@ -51,6 +51,40 @@ class ImageController extends AbstractActionController
     }
         
     public function fileAction() 
-    {        
-    }    
+    {
+        $fileName = $this->params()->fromQuery('name', '');
+
+        $isThumbnail = (bool)$this->params()->fromQuery('thumbnail', false);
+    
+        $fileName = $this->imageManager->getImagePathByName($fileName);
+        
+        if($isThumbnail) {
+            $fileName = $this->imageManager->resizeImage($fileName);
+        }
+                
+        $fileInfo = $this->imageManager->getImageFileInfo($fileName);        
+        if ($fileInfo===false) {
+            $this->getResponse()->setStatusCode(404);            
+            return;
+        }
+                
+        $response = $this->getResponse();
+        $headers = $response->getHeaders();
+        $headers->addHeaderLine("Content-type: " . $fileInfo['type']);        
+        $headers->addHeaderLine("Content-length: " . $fileInfo['size']);
+            
+        $fileContent = $this->imageManager->getImageFileContent($fileName);
+        if($fileContent!==false) {                
+            $response->setContent($fileContent);
+        } else {        
+            $this->getResponse()->setStatusCode(500);
+            return;
+        }
+        
+        if($isThumbnail) {
+            unlink($fileName);
+        }
+        
+        return $this->getResponse();
+    }     
 }
