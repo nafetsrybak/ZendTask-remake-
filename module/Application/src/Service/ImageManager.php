e23e20ca37af8ba2ca527f9c799869a04239c102
nafetsrybak <nafetsrybak@gmail.com>
2019-08-19 20:14:52 +0300

    add function getSavedFiles()

--- module/Application/src/Service/ImageManager.php
+++ module/Application/src/Service/ImageManager.php
@@ -9,5 +9,27 @@ class ImageManager
     public function getSaveToDir() 
     {
         return $this->saveToDir;
-    }  
+    }
+
+    public function getSavedFiles() 
+    {
+        if(!is_dir($this->saveToDir)) {
+            if(!mkdir($this->saveToDir)) {
+                throw new \Exception('Could not create directory for uploads: ' . 
+                             error_get_last());
+            }
+        }
+        
+        $files = [];        
+        $handle  = opendir($this->saveToDir);
+        while (false !== ($entry = readdir($handle))) {
+            
+            if($entry=='.' || $entry=='..')
+                continue;
+            
+            $files[] = $entry;
+        }
+        
+        return $files;
+    } 
 }
