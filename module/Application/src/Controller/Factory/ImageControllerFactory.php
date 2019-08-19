47ba4681b194d7bea5ca9addb40db6954e45d087
nafetsrybak <nafetsrybak@gmail.com>
2019-08-19 20:32:36 +0300

    add ImageController Factory

---
+++ module/Application/src/Controller/Factory/ImageControllerFactory.php
@@ -0,0 +1,18 @@
+<?php
+namespace Application\Controller\Factory;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use Application\Service\ImageManager;
+use Application\Controller\ImageController;
+
+class ImageControllerFactory implements FactoryInterface
+{
+    public function __invoke(ContainerInterface $container, 
+                        $requestedName, array $options = null)
+    {
+        $imageManager = $container->get(ImageManager::class);
+        
+        return new ImageController($imageManager);
+    }
+}
